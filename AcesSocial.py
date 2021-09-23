f0bbd2a7952108a050e10efa133011ab61f64193
Rayan Diniz <84201696+Hagasha@users.noreply.github.com>
2021-09-23 14:17:06 -0300

    Alpha 0.2

--- AcesSocial.py
+++ AcesSocial.py
@@ -7,19 +7,25 @@ class AcesSocial:
         root = Tk()
         root.geometry('320x500+200+100')
         root.title('AcesSocial')
+        root.resizable(True,True)
+        root["bg"] = '#f7f7f7'
+        root.iconbitmap("pessoas.ico")
+        
         logo = PhotoImage(file = "LogoAcesSocial.png")
  
         fr1 = Frame(root, bg= "green")
-        fr1.place(relx = 0.20, rely = 0.20, relwidth = 0.65, relheight = 0.30)
+        fr1.place(relx = 0.20, rely = 0.09, relwidth = 0.65, relheight = 0.30)
  
-        img1 = Label (fr1, image = logo, bg = "gray", activebackground = "green")
+        img1 = Label (fr1, image = logo, bg = "#f7f7f7", activebackground = "green")
         img1.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
+
+        txt1 = Label(root,bg='#f7f7f7', text="Lei de garantias de direitos na Assistência Social", font='arial, 11', height=1, width=5, relief='flat' )
+        txt1.place(relx= 0, rely=0.38, relwidth= 1, relheight= 0.10)
  
         btnInicio = Button(root, bg='#3ec4cd', text='Ir para o aplicativo', font='bold, 10', height=1, width=5, relief='flat')
-        btnInicio.place(relx=0.35, rely=0.60, relwidth = 0.35, relheight = 0.10)
+        btnInicio.place(relx=0.13, rely=0.78, relwidth = 0.75, relheight = 0.10)
  
         root.mainloop()
-
-
- 
+        
+        
 AcesSocial()
